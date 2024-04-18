6934c4fd2dbb4764467883d5366b6f4e97f2790e
MichaelTorres12 <ia.michaeltorrescallejas@gmail.com>
2024-04-18 00:06:49 -0600

    Cree la vista del layout de crear perfil del usuario, donde ingresa los datos de peso, edad, estatura, genero

--- app/src/main/java/com/fitgoalsappsmoviles/fitgoals/activities/ProfileCustomizationActivity.kt
+++ app/src/main/java/com/fitgoalsappsmoviles/fitgoals/activities/ProfileCustomizationActivity.kt
@@ -1,4 +1,12 @@
 package com.fitgoalsappsmoviles.fitgoals.activities
 
-class ProfileCustomizationActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fitgoalsappsmoviles.fitgoals.R
+
+class ProfileCustomizationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_customization)
+    }
 }
